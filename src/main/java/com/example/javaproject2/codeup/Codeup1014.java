3e1669846e8e3eef5c87127e6d68d730efc1a3f7
김현정 <hyeomjeom@gmail.com>
2023-04-20 15:31:26 +0900

    feat : codeup1014 문제 풀이 완료

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1014.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ch1 = sc.next();
+        String ch2 = sc.next();
+
+        System.out.printf("%s %s",ch2, ch1);
+    }
+}
