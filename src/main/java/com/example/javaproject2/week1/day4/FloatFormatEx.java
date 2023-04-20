a4466a015a924a17664bdac2b4011c5b37be3618
김현정 <hyeomjeom@gmail.com>
2023-04-20 13:38:45 +0900

    feat : 3개 숫자 입력 받아 평균 구하기 예제 -FloatFormatEx

---
+++ src/main/java/com/example/javaproject2/week1/day4/FloatFormatEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.2f\n", 1.444);
+        System.out.printf("%.1f\n", (sc.nextFloat() + sc.nextFloat() + sc.nextFloat()) / 3);
+    }
+}
