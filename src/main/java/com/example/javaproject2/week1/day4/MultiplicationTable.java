f7155289cedfdf6ebeb31310e3916f4b160e49b9
김현정 <hyeomjeom@gmail.com>
2023-04-20 13:13:41 +0900

    feat : 구구단 2단 출력 예제

--- src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -4,8 +4,15 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         // 2 * 1 = 2
         // 2 * 2 = 4
-        System.out.println("2 "+"* "+"3 "+"= "+2*3);
-        System.out.printf("%d * %d = %d\n", 2,2,4);
-        System.out.printf("%d * %d = %d", 2, 3, 2 * 3);
+        System.out.printf("%d * %d = %d\n", 2, 1, 2*1);
+        System.out.printf("%d * %d = %d\n", 2, 2 ,2*2);
+        System.out.printf("%d * %d = %d\n", 2, 3 ,2*3);
+        System.out.printf("%d * %d = %d\n", 2, 4 ,2*4);
+        System.out.printf("%d * %d = %d\n", 2, 5 ,2*5);
+        System.out.printf("%d * %d = %d\n", 2, 6 ,2*6);
+        System.out.printf("%d * %d = %d\n", 2, 7 ,2*7);
+        System.out.printf("%d * %d = %d\n", 2, 8 ,2*8);
+        System.out.printf("%d * %d = %d\n", 2, 9 ,2*9);
+
     }
 }
