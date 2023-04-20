013192e39281d1019d3ea9134662dd6f20109ec8
김현정 <hyeomjeom@gmail.com>
2023-04-20 11:27:40 +0900

    fix : 구구단 실습 예제까지 완료

---
+++ src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // 2 * 1 = 2
+        // 2 * 2 = 4
+        System.out.printf("%d * %d = %d", 2,2,4);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfTwoValueShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfTwoValueShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 제가 하고싶은 말은 %s 입니다.", "강사","안녕하세요");
+    }
+}
