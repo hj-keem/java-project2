5d75eea4a31762441bc34b062582f0d8d2bbe6bb
김현정 <hyeomjeom@gmail.com>
2023-04-20 13:40:56 +0900

    feat : Codeup 1013 까지 문제 풀이 완료

--- src/main/java/com/example/javaproject2/codeup/Codeup1006.java
+++ src/main/java/com/example/javaproject2/codeup/Codeup1006.java
@@ -1,6 +1,6 @@
 package com.example.javaproject2.codeup;
 
-public class codeup1006 {
+public class Codeup1006 {
     public static void main(String[] args) {
         System.out.println("\"!@#$%^&*()\"");
     }
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1012.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+import java.util.Scanner;
+public class Codeup1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float fl = sc.nextFloat();
+        System.out.printf("%.6f",fl);
+    }
+}
