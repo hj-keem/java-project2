da69815f9b95b560671c6a2457c39c5c9554c38f
김현정 <hyeomjeom@gmail.com>
2023-04-20 11:32:12 +0900

    fix : 구구단 실습 예제까지 완료

--- src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -4,6 +4,8 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         // 2 * 1 = 2
         // 2 * 2 = 4
-        System.out.printf("%d * %d = %d", 2,2,4);
+        System.out.println("2 "+"* "+"3 "+"= "+2*3);
+        System.out.printf("%d * %d = %d\n", 2,2,4);
+        System.out.printf("%d * %d = %d", 2, 3, 2 * 3);
     }
 }
