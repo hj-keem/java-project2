973c201ad65fe26244b39c817f8353e5830eaf33
김현정 <hyeomjeom@gmail.com>
2023-04-20 11:19:34 +0900

    fix : PrintfEX 파일 업로드

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfEx {
+    public void print() {
+        System.out.printf("%s", "HEllo\n");
+        System.out.println("Bye");
+    }
+
+    public static void main(String[] args){
+        PrintfEx printfEX = new PrintfEx(); // PrintfEX 나와라
+        printfEX.print(); // 물대포 쏴줘 printEX야
+    }
+}
