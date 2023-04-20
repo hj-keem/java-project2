220e86b20ce49f371e9c380a41ff45348fc532ba
김현정 <hyeomjeom@gmail.com>
2023-04-20 11:33:08 +0900

    fix : Codeup 1010번 풀이 완료

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1010.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+import java.util.Scanner;
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
