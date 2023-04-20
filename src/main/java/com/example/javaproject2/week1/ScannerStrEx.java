755ad1c376afbd1e9d4d3e4d09e40d7331631ebc
김현정 <hyeomjeom@gmail.com>
2023-04-20 09:40:49 +0900

    fix : next() 학습 - ScannerStrEx, ScannerStrExTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+import java.util.Scanner;
+public class ScannerStrEx {
+    public void inputStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scan = new ScannerStrEx();
+        scan.inputStringsAndPlus();
+    }
+}
