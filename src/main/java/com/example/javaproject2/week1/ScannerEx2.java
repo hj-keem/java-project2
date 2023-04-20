d6999108f0a5a9f5db0f817812455eed6e64b9f0
김현정 <hyeomjeom@gmail.com>
2023-04-20 09:23:43 +0900

    fix : Scanner 복습 파일 생성 - ScannerEx2 , ScannerExTest2

---
+++ src/main/java/com/example/javaproject2/week1/ScannerEx2.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1;
+import java.util.Scanner;
+
+public class ScannerEx2 {
+
+    public void TwoNumScan(){
+        //숫자 2개 입력
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
