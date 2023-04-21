aaed2aa70f185b62aa2d5972953a1a4e474ba06c
김현정 <hyeomjeom@gmail.com>
2023-04-21 11:46:57 +0900

    feat : 멤버변수 선언 예제 -SpaceInvader 파일 생성

---
+++ src/main/java/com/example/javaproject2/week1/day5/MyAccountEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1.day5;
+
+public class MyAccountEx {
+    public static void main(String[] args) {
+        int sumOfAccount = 1000; // sumOfAccount 라는 변수에 1000이라는 값을 저장
+        System.out.printf("내 통장에는 %d원이 들어있다.\n", sumOfAccount);
+
+        sumOfAccount = 2000; // 저장 값 변경
+        System.out.printf("내 통장에는 %d원이 들어있다. ",sumOfAccount);
+    }
+}
