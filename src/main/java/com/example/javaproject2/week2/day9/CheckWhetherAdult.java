1b72f29774156e7e5a52b2cb564822ef25c5ee98
김현정 <hyeomjeom@gmail.com>
2023-04-27 13:24:05 +0900

    feat : OR,NOT 예제 -CheckWhetherAdult.java, NotEx.java, OrEx.java

--- src/main/java/com/example/javaproject2/week2/day9/CheckWhetherAdult.java
+++ src/main/java/com/example/javaproject2/week2/day9/CheckWhetherAdult.java
@@ -2,8 +2,8 @@ package com.example.javaproject2.week2.day9;
 
 public class CheckWhetherAdult {
     public static void main(String[] args) {
-        int age=18;
-        boolean isAdult = age > 18; //만 18세 이상
-        System.out.println(isAdult == true ? "성인입니다" : "미성년자입니다");
+        int age=30;
+        boolean isAdult = age >= 18; //만 18세 이상
+        System.out.printf("미성년자입니까? %b", !isAdult);
     }
 }
