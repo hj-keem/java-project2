00288368d3c444c5949236defe4d0ef21284ac7d
김현정 <hyeomjeom@gmail.com>
2023-04-27 11:33:05 +0900

    feat : 논리연산자 예제 -LogicalOperator.java

---
+++ src/main/java/com/example/javaproject2/week2/day9/LogicalOperatorEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day9;
+
+public class LogicalOperatorEx {
+    public static void main(String[] args) {
+        int age = 25;
+        boolean isStudent = false;
+
+        if(age >= 18 && !isStudent){ //&& -> AND 연산자로 모두 참인지를 확인
+            System.out.println("성인이며, 학생이 아닙니다.");
+        }
+    }
+}
