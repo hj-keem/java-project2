5c6fef26fccd3f53fa970d66997e24ddae677791
김현정 <hyeomjeom@gmail.com>
2023-05-01 11:47:38 +0900

    feat : Codeup1075까지 문제 풀이

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1073.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<2147483647; i++){
+            int num = sc.nextInt();
+            if(num != 0){
+                System.out.println(num);
+            }else if (num==0){
+                break;
+            }
+
+        }
+    }
+}
