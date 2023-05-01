55a2f25d78e51fb09831f6569e22707f6c3fe9d5
김현정 <hyeomjeom@gmail.com>
2023-05-01 11:32:53 +0900

    feat : 역순 예제 -ForReverse.java

---
+++ src/main/java/com/example/javaproject2/week3/day11/ForReverse.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3.day11;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) { //i--를 해야 내려간다.
+            System.out.println(i);
+//            System.out.printf("%d\n",i); //가로로 출력하기
+        }
+    }
+}
